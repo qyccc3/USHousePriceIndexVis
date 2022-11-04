a7b8c4bbf8b2aac9b1a46fe641d3b97a06eb6c1d
qyccc3 <jackqianhhh@gmail.com>
2022-11-04 14:37:06 -0400

    First Version of USmap.html

--- javascripts/showStates.js
+++ javascripts/showStates.js
@@ -1,7 +1,31 @@
-document.addEventListener('click', function(event){
-    let name = event.target.getAttribute('data-name');
-    if(name != null){
-        document.getElementById('displayName').innerHTML = "<strong>States: </strong>" + name;
-    }
-    console.log(name);
-})
+Promise.all([
+   d3.csv('datasets/cleaned_states_tran_all.csv')
+]).then(ready);
+
+function ready(data){
+    let allowhover = true;
+    document.addEventListener('mouseover', function(event){
+        let name = event.target.getAttribute('data-name');
+        if(name != null && allowhover){
+            document.getElementById('displayName').innerHTML = "<strong>States: </strong>" + name;
+            var result = data[0].filter(obj => {
+                return obj["State Name"] === name;
+            });
+            console.log(result);
+        }
+    })
+    
+    document.addEventListener('click', function(event){
+        if(event.target.getAttribute('data-name') != null){
+            var states = document.querySelectorAll("[id^='state-map']");
+                for (var i = 0; i < states.length; i++) {
+                    states[i].style.fill = "";
+                }
+            event.target.style.cssText += 'fill: yellow;';
+            document.getElementById('displayName').innerHTML = "<strong>States: </strong>" + event.target.getAttribute('data-name');
+            allowhover = false;
+        }
+    })
+};
+
+
